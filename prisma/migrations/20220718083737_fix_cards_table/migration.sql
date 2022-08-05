784c9dfc9573b1a217281ec3f45956875c772019
Rodrigodcs <rodrigodcsee@gmail.com>
2022-08-05 11:04:37 -0300

    Preparando revisão de código

--- prisma/migrations/20220718083737_fix_cards_table/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "cards" ALTER COLUMN "expireIn" SET DATA TYPE TEXT;
--- src/schemas/authSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-import { userData } from "../services/authService.js";
-
-export const userSchema = Joi.object<userData>({
-	email: Joi.string().email().required(),
-	password: Joi.string().min(10).required(),
-});
